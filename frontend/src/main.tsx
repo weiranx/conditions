c75494505ecd59644774e503c21192b3fd2bf619
Weiran Xiong <weiranxiong@Mac.lan>
2026-07-12 15:10:47 -0700

    Optimize mobile interaction experience

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/tokens.css'
 import App from './App.tsx'
+import './styles/mobile-experience.css'
 import { ErrorBoundary } from './components/ErrorBoundary.tsx'
 
 createRoot(document.getElementById('root')!).render(
